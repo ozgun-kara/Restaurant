c613bf53758e5913685e6f6c75a57e9a62c25289
ozgun-kara <ozgunkara_92@hotmail.com>
2019-06-29 14:58:38 +0300

    Restaurant App

---
+++ Restaurant/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Restaurant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(51, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tüm Çalışanları Listele";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(51, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(182, 48);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Menüyü Listele";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(51, 193);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(182, 48);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Şubeleri Listele";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(239, 85);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(249, 141);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(239, 232);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(171, 136);
+            this.listView2.TabIndex = 4;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(525, 320);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(137, 48);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Çıkış";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(119, 247);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(114, 44);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Temizle";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Restaurant.Properties.Resources.Foto1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(712, 411);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Restaurant/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restaurant
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string[,] Tum_Calisanlar = new string[11, 4];
+        public string[,] Tum_Urunler = new string[18, 2];
+        public string[,] Tum_Subeler = new string[1, 2];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // 1. Şubenin çalışanlarının oluşturulması
+
+            Tum_Calisanlar[0, 0] = "John";
+            Tum_Calisanlar[0, 1] = "Smith";
+            Tum_Calisanlar[0, 2] = "0001";
+            Tum_Calisanlar[0, 3] = "Yönetici";
+
+            Tum_Calisanlar[1, 0] = "Frank";
+            Tum_Calisanlar[1, 1] = "Moore";
+            Tum_Calisanlar[1, 2] = "0002";
+            Tum_Calisanlar[1, 3] = "Usta";
+
+            Tum_Calisanlar[2, 0] = "Nilson";
+            Tum_Calisanlar[2, 1] = "Cliff";
+            Tum_Calisanlar[2, 2] = "0003";
+            Tum_Calisanlar[2, 3] = "Usta";
+
+            Tum_Calisanlar[3, 0] = "Kate";
+            Tum_Calisanlar[3, 1] = "Angle";
+            Tum_Calisanlar[3, 2] = "0004";
+            Tum_Calisanlar[3, 3] = "Garson";
+
+            Tum_Calisanlar[4, 0] = "David";
+            Tum_Calisanlar[4, 1] = "Hanks";
+            Tum_Calisanlar[4, 2] = "0005";
+            Tum_Calisanlar[4, 3] = "Garson";
+
+            Tum_Calisanlar[5, 0] = "Chuck";
+            Tum_Calisanlar[5, 1] = "Moore";
+            Tum_Calisanlar[5, 2] = "0006";
+            Tum_Calisanlar[5, 3] = "Garson";
+
+            Tum_Calisanlar[6, 0] = "Terry";
+            Tum_Calisanlar[6, 1] = "Carpenter";
+            Tum_Calisanlar[6, 2] = "0007";
+            Tum_Calisanlar[6, 3] = "Garson";
+
+            Tum_Calisanlar[7, 0] = "Linda";
+            Tum_Calisanlar[7, 1] = "Angle";
+            Tum_Calisanlar[7, 2] = "0008";
+            Tum_Calisanlar[7, 3] = "Kasiyer";
+
+            Tum_Calisanlar[8, 0] = "Denis";
+            Tum_Calisanlar[8, 1] = "Reyes";
+            Tum_Calisanlar[8, 2] = "0009";
+            Tum_Calisanlar[8, 3] = "Kurye";
+
+            Tum_Calisanlar[9, 0] = "Leonardo";
+            Tum_Calisanlar[9, 1] = "Price";
+            Tum_Calisanlar[9, 2] = "0010";
+            Tum_Calisanlar[9, 3] = "Kurye";
+
+            Tum_Calisanlar[10, 0] = "Jason";
+            Tum_Calisanlar[10, 1] = "Nolan";
+            Tum_Calisanlar[10, 2] = "0011";
+            Tum_Calisanlar[10, 3] = "Temizlikçi";
+
+            // Urunlerin oluşturulması
+
+            Tum_Urunler[0, 0] = "Mercimek Çorbası";
+            Tum_Urunler[0, 1] = "137 kcal";
+
+            Tum_Urunler[1, 0] = "Mantar Çorbası";
+            Tum_Urunler[1, 1] = "139 kcal";
+
+            Tum_Urunler[2, 0] = "Tavuk Çorbası";
+            Tum_Urunler[2, 1] = "132 kcal";
+
+            Tum_Urunler[3, 0] = "Spagetti";
+            Tum_Urunler[3, 1] = "382 kcal";
+
+            Tum_Urunler[4, 0] = "Burger";
+            Tum_Urunler[4, 1] = "634 kcal";
+
+            Tum_Urunler[5, 0] = "Lazanya";
+            Tum_Urunler[5, 1] = "692 kcal";
+
+            Tum_Urunler[6, 0] = "Chicken Pane";
+            Tum_Urunler[6, 1] = "540 kcal";
+
+            Tum_Urunler[7, 0] = "Napoli Pizza";
+            Tum_Urunler[7, 1] = "630 kcal";
+
+            Tum_Urunler[8, 0] = "Patates Tabağı";
+            Tum_Urunler[8, 1] = "500 kcal";
+
+            Tum_Urunler[9, 0] = "Soğan Halkası";
+            Tum_Urunler[9, 1] = "380 kcal";
+
+            Tum_Urunler[10, 0] = "Caesar Salad";
+            Tum_Urunler[10, 1] = "98 kcal";
+
+            Tum_Urunler[11, 0] = "Diet Cola";
+            Tum_Urunler[11, 1] = "0 kcal";
+
+            Tum_Urunler[12, 0] = "Cola";
+            Tum_Urunler[12, 1] = "149 kcal";
+
+            Tum_Urunler[13, 0] = "Ice Tea";
+            Tum_Urunler[13, 1] = "99 kcal";
+
+            Tum_Urunler[14, 0] = "Lemonade";
+            Tum_Urunler[14, 1] = "125 kcal";
+
+            Tum_Urunler[15, 0] = "Brownie";
+            Tum_Urunler[15, 1] = "414 kcal";
+
+            Tum_Urunler[16, 0] = "Tiramisu";
+            Tum_Urunler[16, 1] = "452 kcal";
+
+            Tum_Urunler[17, 0] = "Profiterol";
+            Tum_Urunler[17, 1] = "633 kcal";
+
+            // Şubelerin oluşturulması
+
+            Tum_Subeler[0, 0] = "İzmir Şubesi";
+            Tum_Subeler[0, 1] = "001";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+
+            for (int i = 0; i < Tum_Calisanlar.GetLength(0); i++)
+            {
+                for (int j = 0; j < Tum_Calisanlar.GetLength(1); j++)
+                {
+                    listView1.Items.Add(Tum_Calisanlar[i, j]);                                 
+                }
+
+                listView1.Items.Add(""); 
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView2.Items.Clear();
+
+            for (int i = 0; i < Tum_Urunler.GetLength(0); i++)
+            {
+                for (int j = 0; j < Tum_Urunler.GetLength(1); j++)
+                {
+                    listView2.Items.Add(Tum_Urunler[i, j]);
+                }
+
+                listView2.Items.Add("");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listView2.Items.Clear();
+
+            for (int i = 0; i < Tum_Subeler.GetLength(0); i++)
+            {
+                for (int j = 0; j < Tum_Subeler.GetLength(1); j++)
+                {
+                    listView2.Items.Add(Tum_Subeler[i, j]);
+                }
+
+                listView2.Items.Add("");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            listView2.Items.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+    }
+}
